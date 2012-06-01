b8b8083afed0fed7c173f2cc0c2cb28a2c1f7200
Staszek Cieslicki <s.cieslicki@gene-sys.pl>
2012-06-01 10:00:12 +0200

    update KCaptcha to 2.0

--- Vendor/kcaptcha1/form_example.php
+++ Vendor/kcaptcha1/form_example.php
@@ -9,7 +9,7 @@ session_start();
 </form>
 <?php
 if(count($_POST)>0){
-	if(isset($_SESSION['captcha_keystring']) && $_SESSION['captcha_keystring'] === $_POST['keystring']){
+	if(isset($_SESSION['captcha_keystring']) && $_SESSION['captcha_keystring'] == $_POST['keystring']){
 		echo "Correct";
 	}else{
 		echo "Wrong";
